9096ff0e926e74de32bdea8c7412f7e090c3a40b
caduunisal <kadu.unisal1@gmail.com>
2025-03-05 09:56:10 -0300

    Change the way about how to invoke flask - ENTRYPOINT replaced by CMD command (Dockerfile)

--- Python-Flask/app.py
+++ Python-Flask/app.py
@@ -1,53 +1,14 @@
-from flask import Flask, jsonify, request
-from flask_restful import Api, Resource
-
+import os
+from flask import Flask
 app = Flask(__name__)
-api = Api(app)
 
+@app.route('/')
+def main():
+    return 'Welcome'
 
-class Add(Resource):
-    def post(self):
-        # If Im here, then the resource Add was requested using the method POST
+@app.route('/How are you')
+def hello():
+    return 'I am good, how about you?'
 
-        #Step1: Get posted data
-        postedData = request.get_json()
-        x = postedData["x"]
-        y = postedData["y"]
-        x = int(x)
-        y = int(y)
-
-        #Step 2: Add the posted data
-        ret = x + y
-        retMap = {
-            'Message': ret,
-            'Status Code': 200
-        }
-        return jsonify(retMap)
-
-        
-class Subtract(Resource):
-    pass
-
-class Multiply(Resource):
-    pass
-
-class Divide(Resource):
-    pass
-
-
-api.add_resource(Add, "/add")
-
-
-#----------------------------------------------------------------
-#@app.route('/')
-#
-#def hello_world():
-#    return "Hello World! Learning Flask, now!"
-
-
-
-
-#if __name__==__main__:
-#    app.run("host=127.0.0.1", port=80)
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
