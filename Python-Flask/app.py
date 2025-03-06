70c1eb8638edd912de843e9bf4fb6951cebb349e
caduunisal <kadu.unisal1@gmail.com>
2025-03-06 10:35:18 -0300

    Change Flask install instructions

--- Python-Flask/app.py
+++ Python-Flask/app.py
@@ -1,14 +1,7 @@
-import os
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return 'Welcome'
-
-@app.route('/How are you')
-def hello():
-    return 'I am good, how about you?'
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/') 
+def index():
+    return "Hello"
