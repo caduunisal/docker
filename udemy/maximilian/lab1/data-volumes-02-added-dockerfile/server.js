26ad83f1cf079c051ccef5a7a69b18c278a05e3b
caduunisal <kadu.unisal1@gmail.com>
2025-03-10 10:48:27 -0300

    Change server.js to keep volume of input data

--- udemy/maximilian/lab1/data-volumes-02-added-dockerfile/server.js
+++ udemy/maximilian/lab1/data-volumes-02-added-dockerfile/server.js
@@ -36,8 +36,12 @@ app.post('/create', async (req, res) => {
     if (exists) {
       res.redirect('/exists');
     } else {
-      await fs.rename(tempFilePath, finalFilePath);
+      //await fs.rename(tempFilePath, finalFilePath);
+      //res.redirect('/');
+      await fs.copyFile(tempFilePath, finalFilePath);
+      await fs.unlink(tempFilePath);
       res.redirect('/');
+
     }
   });
 });
